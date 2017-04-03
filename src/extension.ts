e8efac7057f0e3c086ca8200c2dd5bdd1a8eccc3
ajtoo <ajtoo.dev@gmail.com>
2017-04-03 16:40:05 -0700

    Implement util function for cursor location detection

--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as Utils from './utils';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -10,7 +11,7 @@ export function activate(context: vscode.ExtensionContext) {
     // Use the console to output diagnostic information (console.log) and errors (console.error)
     // This line of code will only be executed once when your extension is activated
     console.log('Congratulations, your extension "org-mode" is now active!');
-
+    
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
---
+++ src/utils.ts
@@ -0,0 +1,7 @@
+import * as vscode from 'vscode';
+
+export function getCursorPosition() {
+    const curEditor = vscode.window.activeTextEditor;
+    return curEditor.selection.active;;
+}
+
