5688c4d0981543898321c9b958f45eeb5d150f7a
ajtoo <ajtoo.dev@gmail.com>
2017-04-03 16:58:51 -0700

    remove random whitespace

--- src/extension.ts
+++ src/extension.ts
@@ -11,7 +11,7 @@ export function activate(context: vscode.ExtensionContext) {
     // Use the console to output diagnostic information (console.log) and errors (console.error)
     // This line of code will only be executed once when your extension is activated
     console.log('Congratulations, your extension "org-mode" is now active!');
-    
+
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
