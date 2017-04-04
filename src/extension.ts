38f714e3488613ca88247979d575305fd4a8ba6e
ajtoo <ajtoo.dev@gmail.com>
2017-04-04 10:19:27 -0700

    Complete basic sibling insertion

--- src/extension.ts
+++ src/extension.ts
@@ -1,12 +1,9 @@
 'use strict';
 import * as vscode from 'vscode';
-import * as Utils from './utils';
+import * as HeaderFunctions from './header-functions';
 
 export function activate(context: vscode.ExtensionContext) {
-    let insertSiblingCmd = vscode.commands.registerTextEditorCommand('extension.insertSibling', (textEditor, edit) => {
-        Utils.insertText(edit);
-        vscode.window.showInformationMessage('Inserting Sibling');
-    });
+    let insertSiblingCmd = vscode.commands.registerTextEditorCommand('extension.insertSibling', HeaderFunctions.insertSibling);
 
     let insertChildCmd = vscode.commands.registerTextEditorCommand('extension.insertChild', (textEditor, edit) => {
         vscode.window.showInformationMessage('Inserting Child');
