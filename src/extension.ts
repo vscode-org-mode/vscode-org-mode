ecd403b9c960af617eac4cd756614f85d8e6711c
ajtoo <ajtoo.dev@gmail.com>
2017-04-04 21:43:16 -0700

    Finish insert sibling

--- src/extension.ts
+++ src/extension.ts
@@ -5,7 +5,6 @@ import * as TimestampFunctions from './timestamp-functions';
 
 export function activate(context: vscode.ExtensionContext) {
     let insertSiblingCmd = vscode.commands.registerTextEditorCommand('extension.insertSibling', HeaderFunctions.insertSibling);
-
     let insertChildCmd = vscode.commands.registerTextEditorCommand('extension.insertChild', (textEditor, edit) => {
         vscode.window.showInformationMessage('Inserting Child');
     });
--- src/utils.ts
+++ src/utils.ts
@@ -5,10 +5,6 @@ export function getCursorPosition() {
     return curEditor.selection.active;
 }
 
-export function insertText(edit: vscode.TextEditorEdit, position: vscode.Position, text: string) {
-    const document = vscode.window.activeTextEditor.document;
-    edit.insert(position, text);     //lead with \n to place on next line
-}
 
 export function getActiveTextEditorEdit() {
     return vscode.window.activeTextEditor.document;
@@ -19,9 +15,51 @@ export function getLine(document: vscode.TextDocument, lineNum: vscode.Position)
 }
 
 export function getHeaderPrefix(line: string) {
-    return line.match(/^\*+/);
+    const prefix = line.match(/^\*+/);
+    if(prefix)
+        return prefix[0];
+    else
+        return "";
 }
 
-export function findPreviousHeader() {
-    
+
+export function getPrefix(line: string) {
+    const prefix = line.match(/^[(\*)(-)(\d+.)]+/);
+    if(prefix)
+        return prefix[0];
+    else
+        return "";
+}
+
+export function findParentPrefix(document: vscode.TextDocument, pos: vscode.Position) {
+    let thisLinePrefix = getHeaderPrefix(getLine(document, pos));
+    let curLine = pos.line;
+    let curLinePrefix = "";
+
+    //figure out why it goes all the way up to single star
+    while(curLine >= 0 && curLinePrefix == thisLinePrefix) {
+        curLine--;
+        let curLineContent = getLine(document, new vscode.Position(curLine, 0));
+        curLinePrefix = getHeaderPrefix(curLineContent);
+    }
+
+    return curLinePrefix;
+}
+
+export function findEndOfSection(document: vscode.TextDocument, pos: vscode.Position, levelSym: string = "") {
+    let curLine = pos.line;
+    let curPos = new vscode.Position(pos.line, 0);      //set to line: curLine and character: <end of line>
+    let curLinePrefix = levelSym;
+
+    while(curLine <= document.lineCount && curLinePrefix == levelSym) {
+        curLine++;
+        curPos = new vscode.Position(curLine, 0);
+        let curLineContent = getLine(document, curPos);
+        curLinePrefix = getPrefix(curLineContent);
+    }
+
+
+    curPos = new vscode.Position(curPos.line - 1, getLine(document, new vscode.Position(curPos.line - 1, 0)).length + 1);
+
+    return curPos;
 }
