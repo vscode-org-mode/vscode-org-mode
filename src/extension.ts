8997dd071f984a2e213d298ae23f8f9c81fc4853
ajtoo <ajtoo.dev@gmail.com>
2017-04-05 02:42:29 -0700

    Implement basic promote

--- src/extension.ts
+++ src/extension.ts
@@ -8,6 +8,7 @@ export function activate(context: vscode.ExtensionContext) {
     let insertSiblingCmd = vscode.commands.registerTextEditorCommand('extension.insertSibling', HeaderFunctions.insertSibling);
     let insertChildCmd = vscode.commands.registerTextEditorCommand('extension.insertChild', HeaderFunctions.insertChild);
     let demoteLineCmd = vscode.commands.registerTextEditorCommand('extension.demoteLine', HeaderFunctions.demoteLine);
+    let promoteLineCmd = vscode.commands.registerTextEditorCommand('extension.promoteLine', HeaderFunctions.promoteLine);
 
     let insertTimestampCmd = vscode.commands.registerTextEditorCommand('extension.insertTimestamp', (textEditor, edit) => {
       vscode.window.showInformationMessage('Inserting Date');
@@ -24,6 +25,7 @@ export function activate(context: vscode.ExtensionContext) {
     context.subscriptions.push(insertSiblingCmd);
     context.subscriptions.push(insertChildCmd);
     context.subscriptions.push(demoteLineCmd);
+    context.subscriptions.push(promoteLineCmd);
     context.subscriptions.push(insertTimestampCmd);
     context.subscriptions.push(boldCmd);
     context.subscriptions.push(italicCmd);
