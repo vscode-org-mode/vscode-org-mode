6901122f7a87517e0e413d6bf71a22603bd68506
Gilbert <PGilbertSchmitt@gmail.com>
2017-04-04 21:21:23 -0700

    Rename date to timestamp

--- src/timestamp-functions.ts
+++ src/timestamp-functions.ts
@@ -2,7 +2,7 @@ import * as vscode from 'vscode';
 import * as Utils from './utils';
 import * as Datetime from './simple-datetime';
 
-export function insertDate(textEditor: vscode.TextEditor, edit: vscode.TextEditorEdit) {
+export function insertTimestamp(textEditor: vscode.TextEditor, edit: vscode.TextEditorEdit) {
     const document = Utils.getActiveTextEditorEdit();
     const cursorPos = Utils.getCursorPosition();
     
