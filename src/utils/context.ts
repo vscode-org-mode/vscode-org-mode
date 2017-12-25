3bda412a198ff82848c423221745abb30d2e6de9
ajtoo <ajtoo.dev@gmail.com>
2017-12-25 04:28:03 -0800

    Flesh out section functions as a trial (needs testing)

--- src/utils/context.ts
+++ src/utils/context.ts
@@ -9,6 +9,7 @@ import {
 } from 'vscode';
 
 class CursorContext {
+    private document: TextDocument;
     public cursorPos: Position;
     public curLine: string;
     public prefix: string;
@@ -16,9 +17,56 @@ class CursorContext {
 
     constructor(document: TextDocument, cursorPos: Position) {
         this.cursorPos = cursorPos;
-        this.curLine = document.lineAt(this.cursorPos).text;;
+        this.curLine = document.lineAt(this.cursorPos).text;
+        this.document = document;
         this.prefix = this.getPrefix(this.curLine);
         this.isHeader = this.prefix.startsWith("*");
+    }
+
+    public findBeginningOfSection() {
+        let curLine = this.cursorPos.line;
+        let curPos;
+        let curLinePrefix;
+    
+        do {
+            curLine--;
+            curPos = new Position(curLine, 0);
+            curLinePrefix = this.getPrefix(this.document.lineAt(curPos).text);
+        } while(curLine > 0 && this.inSubsection(curLinePrefix))
+    
+        if(curPos) {
+            curPos = new Position(curPos.line + 1, 0);
+        }
+    
+        return curPos;
+    }
+
+    public findEndOfSection() {
+        if(this.cursorPos.line === this.document.lineCount - 1) {
+            return new Position(this.cursorPos.line, this.curLine.length);
+        }
+    
+        let curLine = this.cursorPos.line;
+        let curPos;
+        let curLinePrefix;
+    
+        do {
+            curLine++;
+            curPos = new Position(curLine, 0);
+            curLinePrefix = this.getPrefix(this.document.lineAt(curPos).text);
+        } while(curLine < this.document.lineCount - 1 && this.inSubsection(curLinePrefix))
+
+        curPos = new Position(curPos.line - 1, this.getLine(new Position(curPos.line - 1, 0)).length + 1);
+    
+        return curPos;
+    }
+
+    private inSubsection(linePrefix: string) {
+        return this.prefix === linePrefix || linePrefix === "-" || !linePrefix || linePrefix.match(/\d+\./);
+    }
+
+    private getLine(pos: Position) {
+        return this.document.lineAt(pos).text;
     }
 
     private getPrefix(line: string) {
