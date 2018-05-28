0ee1ac2621601a47711983e46e483ab906e7c6de
AdrieanKhisbe <adriean.khisbe@live.fr>
2018-05-29 00:45:06 +0200

    Implement folding for #+BEGIN/#+END blocks :tada:

--- src/org-folding-provider.ts
+++ src/org-folding-provider.ts
@@ -14,27 +14,31 @@ export class OrgFoldingProvider implements FoldingRangeProvider {
 
         let stack: FoldStart[] = [];
         let ranges: FoldingRange[] = [];
+        let blockStack: number = null;
 
         for (let lineNumber = 0; lineNumber < count; lineNumber++) {
             const element = document.lineAt(lineNumber);
             const text = element.text;
 
-            if (!text.startsWith("*")) {
-                continue;
-            }
+            if (text.startsWith('*')) {
+                let level = 0;
+                while (text[level] === '*') {
+                    level++;
+                }
 
-            let level = 0;
-            while (text[level] === '*') {
-                level++;
-            }
+                const adjustmentCount = stack.length - level + 1;
+                for (let i = 0; i < adjustmentCount; i++) {
+                    const top = stack.pop();
+                    ranges.push(new FoldingRange(top.lineNumber, lineNumber - 1));
+                }
 
-            const adjustmentCount = stack.length - level + 1;
-            for (let i = 0; i < adjustmentCount; i++) {
-                const top = stack.pop();
-                ranges.push(new FoldingRange(top.lineNumber, lineNumber - 1));
+                stack.push({ level, lineNumber });
+            } else if (/^\s*#\+(BEGIN|END)_/.test(text)) {
+                if (text.startsWith('#+BEGIN_')) { blockStack = lineNumber; }
+                else {
+                    ranges.push(new FoldingRange(blockStack, lineNumber));
+                }
             }
-
-            stack.push({ level, lineNumber });
         }
 
         let top: FoldStart;
