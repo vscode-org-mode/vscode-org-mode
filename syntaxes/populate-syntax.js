29afc16afd9b791fa4bacc5bf1c7c875763627d8
AdrieanKhisbe <adriean.khisbe@live.fr>
2018-05-29 00:14:08 +0200

    Handling of a default pattern when source language is not defined

--- syntaxes/populate-syntax.js
+++ syntaxes/populate-syntax.js
@@ -7,38 +7,46 @@ const languages = [
     ['java'],
     ['python', 'py']
 ];
+const generateDefinitionForLanguage = language =>
+    generateBlockSourceDefinition(language[0], language.join('|'), `source.${language[0]}`)
 
-const generateDefinitionForLanguage = language => ({
-    "name": `meta.block.source.${language[0]}.org`,
-    "begin": `(?i)(#\\+BEGIN_SRC)\\s+(${language.join('|')})\\b\\s*(.*)$`,
-    "end": "(?i)(#\\+END_SRC)$",
-    "beginCaptures": {
-        "1": {
-            "name": "keyword.control.block.org"
+const generateBlockSourceDefinition = (scope, match, sourceLanguage) => {
+    var basePattern = {
+        name: `meta.block.source.${scope}.org`,
+        begin: `(?i)(#\\+BEGIN_SRC)\\s+(${match})\\b\\s*(.*)$`,
+        end: "(?i)(#\\+END_SRC)$",
+        beginCaptures: {
+            "1": {
+                "name": "keyword.control.block.org"
+            },
+            "2": {
+                "name": "constant.other.language.org"
+            },
+            "3": {
+                "name": "string.other.header-args.org"
+            }
         },
-        "2": {
-            "name": "constant.other.language.org"
-        },
-        "3": {
-            "name": "string.other.header-args.org"
+        endCaptures: {
+            "1": {
+                "name": "keyword.control.block.org"
+            }
         }
-    },
-    "endCaptures": {
-        "1": {
-            "name": "keyword.control.block.org"
-        }
-    },
-    "patterns": [
-        {
-            "include": `source.${language[0]}`
-        }
-    ]
-});
+    }
+    if (sourceLanguage)
+        basePattern.patterns = [
+            {
+                "include": sourceLanguage
+            }
+        ];
+    return basePattern;
+};
 
 const generateGrammar = () => {
     const grammarTemplate = fs.readFileSync(`${__dirname}/org.tmLanguage.template.json`);
     const jsonGrammar = JSON.parse(grammarTemplate);
-    jsonGrammar.repository['src-block'].patterns = languages.map(generateDefinitionForLanguage);
+    const languageDefinitions = languages.map(generateDefinitionForLanguage)
+    languageDefinitions.push(generateBlockSourceDefinition('unknown', '\\w+', null))
+    jsonGrammar.repository['src-block'].patterns = languageDefinitions;
     fs.writeFileSync(`${__dirname}/org.tmLanguage.json`, JSON.stringify(jsonGrammar, null, 4));
 };
 
