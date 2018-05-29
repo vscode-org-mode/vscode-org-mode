64e7bfacd6f06e0e73811566366e03de65f7d82a
AdrieanKhisbe <adriean.khisbe@live.fr>
2018-05-29 06:27:32 +0200

    Add support for embedded markup languages

--- syntaxes/populate-syntax.js
+++ syntaxes/populate-syntax.js
@@ -1,14 +1,27 @@
 // Script that populate syntax with list of language
 const fs = require('fs');
 
-const languages = [
+const programmingLanguages = [
     ['js', 'javascript'],
     ['ts', 'typescript'],
     ['java'],
-    ['python', 'py']
+    ['python', 'py', 'gyp']
 ];
-const generateDefinitionForLanguage = language =>
-    generateBlockSourceDefinition(language[0], language.join('|'), `source.${language[0]}`)
+
+const markupLanguages = [
+    ['source.yaml', 'yaml', 'yml'],
+    ['source.json', 'json'],
+    ['text.xml', 'xml'],
+    ['text.html.markdown', 'markdown', 'md'],
+    ['source.org', 'orgmode', 'org'],
+    ['text.html.basic', 'html', 'htm', 'shtml', 'xhtml', 'inc', 'tmpl', 'tpl']
+];
+
+const generateDefinitionForProgrammingLanguage = language =>
+    generateBlockSourceDefinition(language[0], language.join('|'), `source.${language[0]}`);
+
+const generateDefinitionForMarkupLanguage = language =>
+    generateBlockSourceDefinition(language[1], language.slice(1).join('|'), language[0]);
 
 const generateBlockSourceDefinition = (scope, match, sourceLanguage) => {
     var basePattern = {
@@ -51,7 +64,8 @@ const generateBlockSourceDefinition = (scope, match, sourceLanguage) => {
 const generateGrammar = () => {
     const grammarTemplate = fs.readFileSync(`${__dirname}/org.tmLanguage.template.json`);
     const jsonGrammar = JSON.parse(grammarTemplate);
-    const languageDefinitions = languages.map(generateDefinitionForLanguage)
+    const languageDefinitions = programmingLanguages.map(generateDefinitionForProgrammingLanguage)
+        .concat(markupLanguages.map(generateDefinitionForMarkupLanguage));
     languageDefinitions.push(generateBlockSourceDefinition('unknown', '\\w+', null))
     jsonGrammar.repository['src-block'].patterns = languageDefinitions;
     fs.writeFileSync(`${__dirname}/org.tmLanguage.json`, JSON.stringify(jsonGrammar, null, 4));
