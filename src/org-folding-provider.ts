199d076a212b5bbf44f959378974c71c128ad6f2
Adrien Becchis <adrien.becchis@gmail.com>
2018-09-11 21:34:45 +0200

    Feature/source block support (#108)
    * Prototype of source blocks for javascript only
    
    * Prototype of source block section generation per language
    
    * Tweaks to keyword definition that was too greedy
    
    * Handling of a default pattern when source language is not defined
    
    * Implement folding for #+BEGIN/#+END blocks :tada:
    
    * Remove folding known issue that has been solved
    
    * Add `meta.embedded.block.${scope}` scope to code content
    
    * Add support for embedded markup languages
    
    * Add definition for almost all the language and generate grammar
    
    by all language, is meant all supported in https://github.com/Microsoft/vscode/blob/master/extensions/markdown-basics/syntaxes/markdown.tmLanguage.json
    
    * Introduce generic block grammar definition with embeded common org syntax
    
    * Block must be preceeded only by trailing spaces
    
    * Diverses tweaks, adding also lisp to the syntaxes :wink:
    
    * Add latex to the supported grammar of SRC block
    
    * Fix the nested pattern regexp while expression to be case insensitive
    
    * Support trailing whitespace at the end of a source block

--- src/org-folding-provider.ts
+++ src/org-folding-provider.ts
@@ -14,27 +14,31 @@ export class OrgFoldingProvider implements FoldingRangeProvider {
 
         let stack: FoldStart[] = [];
         let ranges: FoldingRange[] = [];
+        let blockStack: number = null;
 
         for (let lineNumber = 0; lineNumber < count; lineNumber++) {
             const element = document.lineAt(lineNumber);
             const text = element.text;
 
-            if (!text.startsWith("*")) {
-                continue;
-            }
+            if (text.startsWith('*')) {
+                let level = 0;
+                while (text[level] === '*') {
+                    level++;
+                }
 
-            let level = 0;
-            while (text[level] === '*') {
-                level++;
-            }
+                const adjustmentCount = stack.length - level + 1;
+                for (let i = 0; i < adjustmentCount; i++) {
+                    const top = stack.pop();
+                    ranges.push(new FoldingRange(top.lineNumber, lineNumber - 1));
+                }
 
-            const adjustmentCount = stack.length - level + 1;
-            for (let i = 0; i < adjustmentCount; i++) {
-                const top = stack.pop();
-                ranges.push(new FoldingRange(top.lineNumber, lineNumber - 1));
-            }
+                stack.push({ level, lineNumber });
 
-            stack.push({ level, lineNumber });
+            } else if (/^\s*#\+BEGIN_/.test(text)) {
+                blockStack = lineNumber;
+            } else if (/^\s*#\+END_/.test(text)) {
+                ranges.push(new FoldingRange(blockStack, lineNumber));
+            }
         }
 
         let top: FoldStart;
