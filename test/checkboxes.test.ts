c3937a657559ef6c2c59d8b2166ebb731ee31ffe
Alexei Boukirev <aboukirev@gmail.com>
2018-10-09 14:03:44 -0500

    Added test ticking parent checkbox ticks all children

--- test/checkboxes.test.ts
+++ test/checkboxes.test.ts
@@ -43,4 +43,29 @@ suite('Checkboxes', () => {
             assert.equal(actual, expected);
         }).then(done, done);
     });
+    test('Ticking parent checkbox ticks all children', done => {
+        const filePath = join(__dirname, '../../test/fixtures/checkboxes.org');
+        const expected = [
+            '    - [x] updates parent summary/percent cookie',
+            '    - [x] sets parent to on if all children are on',
+            '    - [x] sets parent to off when all children are off',
+            '    - [x] sets parent to undetermined when some children are on and some are off'
+        ];
+        let textDocument: TextDocument;
+        workspace.openTextDocument(filePath).then(document => {
+            textDocument = document;
+            return window.showTextDocument(document);
+        }).then(editor => {
+            const pos = new Position(12, 14);
+            editor.selection = new Selection(pos, pos);
+            return editor.edit(edit => {
+                checkboxes.OrgToggleCheckbox(editor, edit);
+            });
+        }).then(() => {
+            for (var i: number = 0; i < 4; i++) {
+                var actual = textDocument.lineAt(13 + i).text;
+                assert.equal(actual, expected[i]);
+            }
+        }).then(done, done);
+    });
 });
