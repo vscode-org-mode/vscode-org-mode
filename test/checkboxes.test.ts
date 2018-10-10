93cc88424a4b73a70c8a0a5e96d080efa93e7786
Alexei Boukirev <aboukirev@gmail.com>
2018-10-10 11:11:49 -0500

    Use async instead of promises in tests

--- test/checkboxes.test.ts
+++ test/checkboxes.test.ts
@@ -72,103 +72,68 @@ suite('Checkboxes', () => {
         }
         done();
     });
-    test('Can update summary', done => {
+    test('Can update summary', async () => {
         let expected = '* TODO Implement tests [0%] [0/4]';
-        let textDocument: vscode.TextDocument;
-        loadContent().then(document => {
-            textDocument = document;
-            return vscode.window.showTextDocument(document);
-        }).then(editor => {
-            moveAndSelect(editor, 9, 5);
-            return editor.edit(edit => {
-                checkboxes.OrgUpdateSummary(editor, edit);
-            });
-        }).then(() => {
-            var actual = textDocument.lineAt(9).text;
-            assert.equal(actual, expected);
-            return Promise.resolve();
-        }).then(done, done);
+        let document = await loadContent();
+        let editor = await vscode.window.showTextDocument(document);
+        let registration = vscode.commands.registerTextEditorCommand('org.updateSummary', checkboxes.OrgUpdateSummary);
+        moveAndSelect(editor, 9, 5);
+        await vscode.commands.executeCommand('org.updateSummary');
+        var actual = document.lineAt(9).text;
+        assert.equal(actual, expected);
+        registration.dispose();
     });
-    test('Ticking checkbox updates parent', done => {
+    test('Ticking checkbox updates parent', async () => {
         let expected = '  - [-] toggling child checkbox [25%]';
-        let textDocument: vscode.TextDocument;
-        loadContent().then(document => {
-            textDocument = document;
-            return vscode.window.showTextDocument(document);
-        }).then(editor => {
-            moveAndSelect(editor, 14, 14);
-            return editor.edit(edit => {
-                checkboxes.OrgToggleCheckbox(editor, edit);
-            });
-        }).then(() => {
-            var actual = textDocument.lineAt(12).text;
-            assert.equal(actual, expected);
-            return Promise.resolve();
-        }).then(done, done);
+        let document = await loadContent();
+        let editor = await vscode.window.showTextDocument(document);
+        let registration = vscode.commands.registerTextEditorCommand('org.toggleCheckbox', checkboxes.OrgToggleCheckbox);
+        moveAndSelect(editor, 14, 14);
+        await vscode.commands.executeCommand('org.toggleCheckbox');
+        var actual = document.lineAt(12).text;
+        assert.equal(actual, expected);
+        registration.dispose();
     });
-    test('Ticking parent checkbox ticks all children', done => {
+    test('Ticking parent checkbox ticks all children', async () => {
         const expected = [
             '    - [x] updates parent summary/percent cookie',
             '    - [x] sets parent to on if all children are on',
             '    - [x] sets parent to off when all children are off',
             '    - [x] sets parent to undetermined when some children are on and some are off'
         ];
-        let textDocument: vscode.TextDocument;
-        loadContent().then(document => {
-            textDocument = document;
-            return vscode.window.showTextDocument(document);
-        }).then(editor => {
-            moveAndSelect(editor, 12, 14);
-            return editor.edit(edit => {
-                checkboxes.OrgToggleCheckbox(editor, edit);
-            });
-        }).then(() => {
-            for (var i: number = 0; i < 4; i++) {
-                var actual = textDocument.lineAt(13 + i).text;
-                assert.equal(actual, expected[i]);
-            }
-            return Promise.resolve();
-        }).then(done, done);
+        let document = await loadContent();
+        let editor = await vscode.window.showTextDocument(document);
+        let registration = vscode.commands.registerTextEditorCommand('org.toggleCheckbox', checkboxes.OrgToggleCheckbox);
+        moveAndSelect(editor, 12, 14);
+        await vscode.commands.executeCommand('org.toggleCheckbox');
+        for (var i: number = 0; i < 4; i++) {
+            var actual = document.lineAt(13 + i).text;
+            assert.equal(actual, expected[i]);
+        }
+        registration.dispose();
     });
-    test('Unticking last ticked child clears parent checkbox', done => {
+    test('Unticking last ticked child clears parent checkbox', async () => {
         let expected = '  - [ ] call people [0/3]';
-        let textDocument: vscode.TextDocument;
-        loadContent().then(document => {
-            textDocument = document;
-            return vscode.window.showTextDocument(document);
-        }).then(editor => {
-            moveAndSelect(editor, 3, 14);
-            return editor.edit(edit => {
-                checkboxes.OrgToggleCheckbox(editor, edit);
-            });
-        }).then(() => {
-            var actual = textDocument.lineAt(1).text;
-            assert.equal(actual, expected);
-            return Promise.resolve();
-        }).then(done, done);
+        let document = await loadContent();
+        let editor = await vscode.window.showTextDocument(document);
+        let registration = vscode.commands.registerTextEditorCommand('org.toggleCheckbox', checkboxes.OrgToggleCheckbox);
+        moveAndSelect(editor, 3, 14);
+        await vscode.commands.executeCommand('org.toggleCheckbox');
+        var actual = document.lineAt(1).text;
+        assert.equal(actual, expected);
+        registration.dispose();
     });
-    test('Ticking all children ticks parent checkbox', done => {
+    test('Ticking all children ticks parent checkbox', async () => {
         let expected = '  - [x] toggling parent checkbox [3/3]';
-        let textDocument: vscode.TextDocument;
-        let textEditor: vscode.TextEditor;
-        loadContent().then(document => {
-            textDocument = document;
-            return vscode.window.showTextDocument(document);
-        }).then(editor => {
-            textEditor = editor;
-            moveAndSelect(editor, 18, 5);
-            return editor.edit(edit => {
-                checkboxes.OrgToggleCheckbox(editor, edit);
-            });
-        }).then(() => {
-            moveAndSelect(textEditor, 20, 5);
-            return textEditor.edit(edit => {
-                checkboxes.OrgToggleCheckbox(textEditor, edit);
-            });
-        }).then(() => {
-            var actual = textDocument.lineAt(17).text;
-            assert.equal(actual, expected);
-            return Promise.resolve();
-        }).then(done, done);
+        let document = await loadContent();
+        let editor = await vscode.window.showTextDocument(document);
+        let registration = vscode.commands.registerTextEditorCommand('org.toggleCheckbox', checkboxes.OrgToggleCheckbox);
+        moveAndSelect(editor, 18, 5);
+        await vscode.commands.executeCommand('org.toggleCheckbox');
+        moveAndSelect(editor, 20, 5);
+        await vscode.commands.executeCommand('org.toggleCheckbox');
+        var actual = document.lineAt(17).text;
+        assert.equal(actual, expected);
+        registration.dispose();
     });
 });
