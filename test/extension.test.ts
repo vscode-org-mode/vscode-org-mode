cd1f804e2c94eed41e8d5315fb72b17edd37ad4a
Roman Peshkov <peshkovroman@gmail.com>
2018-10-17 02:30:48 +0200

    Code coverage configuration

--- test/extension.test.ts
+++ test/extension.test.ts
@@ -9,14 +9,18 @@ import * as assert from 'assert';
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
 import * as vscode from 'vscode';
-import * as myExtension from '../src/extension';
+
+import * as utils from '../src/utils';
+
 
 // Defines a Mocha test suite to group tests of similar kind together
 suite("Extension Tests", () => {
 
     // Defines a Mocha unit test
-    test("Something 1", () => {
-        assert.equal(-1, [1, 2, 3].indexOf(5));
-        assert.equal(-1, [1, 2, 3].indexOf(0));
+    test("Dummy test", async () => {
+        const expected = '***';
+        const headerPrefix = utils.getHeaderPrefix('*** Header');
+
+        assert.equal(headerPrefix, expected);
     });
 });
