7c9043594817fd1b22c9708d899de40b558b40c7
Roman Peshkov <peshkovroman@gmail.com>
2019-02-24 10:17:07 +0100

    Code coverage setup (#128)
    * Readme Buttons (#1)
    
    * Code coverage configuration
    
    * Test runner extracted to NPM package

--- test/index.ts
+++ test/index.ts
@@ -10,7 +10,7 @@
 // to report the results back to the caller. When the tests are finished, return
 // a possible error to the callback or null if none.
 
-var testRunner = require('vscode/lib/testrunner');
+var testRunner = require('vscode-coverage-test-runner');
 
 // You can directly control Mocha options by uncommenting the following lines
 // See https://github.com/mochajs/mocha/wiki/Using-mocha-programmatically#set-options for more info
